67be5ae53454baab79eaf7b20df8177545960ec9
bt853210 <bt853210@gmail.com>
2019-05-24 10:59:05 +0800

    1

--- app/src/main/java/com/example/score/MainActivity.java
+++ app/src/main/java/com/example/score/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.score;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText etPrograming,etDataStructure,etAlgorithm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+    }
+    private void findViews(){
+        etPrograming = (EditText)findViewById(R.id.etProgramming);
+        etDataStructure=(EditText)findViewById(R.id.etDataStructure);
+        etAlgorithm=(EditText)findViewById(R.id.etAlgorithm);
+    }
+    private boolean isValid(EditText editText){
+        String pattern = "1[0]{2}|[0-9]{1,2}";
+        String text = editText.getText().toString();
+        if(!text.matches(pattern)){
+            editText.setError("0~100");
+            return false;
+        }
+        else return true;
     }
 }
