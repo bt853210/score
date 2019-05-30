7642d817afd4c997bcf37fbdd79fa9e1dc9d7743
bt853210 <bt853210@gmail.com>
2019-05-30 13:01:21 +0800

    4

--- app/src/main/java/com/example/score/ResultActivitity.java
+++ app/src/main/java/com/example/score/ResultActivitity.java
@@ -1,6 +1,7 @@
 package com.example.score;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
@@ -26,6 +27,39 @@ public class ResultActivitity extends AppCompatActivity {
         double average = sum /3.0;
         String text = "programming="+programming+"\ndataStructure="+dataStructure+"\nalgorithm"+algorithm+"\nsum"+sum+"\naverage"+nf.format(average);
         tvResult.setText(text);
+        alert(average);
     }
+    private void alert(double average){
+        String message = new String();
+        String title = new String();
+        int pic=0;
+
+        if(average==100){
+            message="100";
+            title="Pass";
+            pic=R.drawable.circle;
+        }
+        else if(average>=60){
+            message="60";
+            title="Pass";
+            pic=R.drawable.circle;
+        }
+        if(average==40){
+            message="100";
+            title="failed";
+            pic=R.drawable.tan;
+        }
+        else {
+            message="0";
+            title="failed";
+            pic=R.drawable.X;
+        }
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setMessage(message);
+        builder.setTitle(title);
+        builder.setIcon(pic);
+        builder.show();
+    }
+
     public void onBackClick(View view){finish();}
 }
